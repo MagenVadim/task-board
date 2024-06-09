0624c1e80bb5e186746a9e79e56fbde91a24e395
Vadim Smagin <magenvadim@gmail.com>
2024-06-09 23:40:49 +0300

    #26 userReducer has connected to users-dashboard

--- src/pages/UsersDashboard/UsersDashboard.jsx
+++ src/pages/UsersDashboard/UsersDashboard.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
 
 export const UsersDashboard = () => {
+
+  const userStore = useSelector(state => state.userReducer)
+
   return (
-    <div>UsersDashboard</div>
+    <div>
+      {userStore.map(user=>
+        <p>{user.fullName}</p>
+      )}
+    </div>
   )
 }
