4d08b6ed02afc5cc8aeffcd5da4fc6e2be6a76dc
Vadim Smagin <magenvadim@gmail.com>
2024-06-16 23:45:02 +0300

    #32 users-header has added

--- src/pages/UsersDashboard/UsersDashboard.jsx
+++ src/pages/UsersDashboard/UsersDashboard.jsx
@@ -9,36 +9,45 @@ export const UsersDashboard = () => {
   const userStore = useSelector(state => state.userReducer)
 
   return (
-    <div className="users-user-list">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-          {userStore.map(user=>
-            <tr key={user.fullName}>
-              <td className="users-table">
-                <Link className="user-link" to={`/users/card/${user.fullName}`}>
-                  <div className="users-user-card">
-                    <img src={user.url} alt="task-user" className="thumb-sm rounded-circle mr-2"/>
-                    <p >{user.fullName}</p>
-                  </div>
-                </Link>
-              </td>
-              <td className="details">
-                <p className="user-details">{user.position}</p>
-              </td>
-              <td className="details">
-                <p className="user-details">{user.city}</p>
-              </td>
+    <>
+      <div className="users-header">
+        
+
+      </div>
+
+
+      <div className="users-user-list">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Position</th>
+              <th>City</th>
             </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {userStore.map(user=>
+              <tr key={user.fullName}>
+                <td className="users-table">
+                  <Link className="user-link" to={`/users/card/${user.fullName}`}>
+                    <div className="users-user-card">
+                      <img src={user.url} alt="task-user" className="thumb-sm rounded-circle mr-2"/>
+                      <p >{user.fullName}</p>
+                    </div>
+                  </Link>
+                </td>
+                <td className="details">
+                  <p className="user-details">{user.position}</p>
+                </td>
+                <td className="details">
+                  <p className="user-details">{user.city}</p>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+    
   )
 }
